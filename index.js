b9d8fd72baf3144dd9c3ec33ed4b8f23ed9eba37
NotKobe <al.10549329@gmail.com>
2022-04-30 22:00:31 -0500

    Added comments/documentation for panels

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const panels = document.querySelectorAll(".panel");
+/* All of this code is used for the panels that are used for our users reviews about how great our app is 
+:]
+*/
+
+const panels = document.querySelectorAll(".panel"); //In the CSS file panels is the class they are called
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
